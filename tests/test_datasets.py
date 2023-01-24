7cc0a2a48771c502f6bdc51a71be0cbe5e775030
Frédéric St-Onge <64271866+stong3@users.noreply.github.com>
2023-01-24 09:45:39 -0500

    tests: updated tests to match the new datasets function

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,11 +1,14 @@
 import pytest
+import os
 from sihnpy import datasets
 
-def test_pad_conp_conn_minimal_dataset():
+def test_pad_fp_input():
     """ Function testing that the import of data is done right.
     """
 
-    participants_data, paths_conn = datasets.pad_conp_conn_minimal_dataset()
+    participants_data, path_ids, paths_conn = datasets.pad_fp_input()
 
-    assert len(participants_data)  == 4, "Wrong number of columns for participants.tsv"
-    assert paths_conn['BL00']['rest_run1'] == '/Users/fredericst-onge/Desktop/sihnpy/src/sihnpy/data/pad_conp_minimal/BL00/rest_run1', "Dictionary not formatted correctly"
+    assert os.path.exists(path_ids), "Path to participants' ids is broken"
+    assert len(participants_data)  == 15, "Wrong number of participants in participants.tsv"
+    assert len(paths_conn) == 2, "Wrong number of follow-ups for dictionary"
+    assert os.path.exists(paths_conn['BL00']['rest_run1']), "Paths to modalities are broken"
