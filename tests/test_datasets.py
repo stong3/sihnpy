e5343c7b1fcfaf9e6e02f5a90da376cddff8f7a2
Frédéric St-Onge <64271866+stong3@users.noreply.github.com>
2023-03-20 16:49:43 -0400

    tests: add test for spatial extent tests loading

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import pandas as pd
 from sihnpy import datasets
 
 def test_pad_fp_input():
@@ -12,3 +13,14 @@ def test_pad_fp_input():
     assert len(participants_data)  == 15, "Wrong number of participants in participants.tsv"
     assert len(paths_conn) == 2, "Wrong number of follow-ups for dictionary"
     assert os.path.exists(paths_conn['BL00']['rest_run1']), "Paths to modalities are broken"
+
+def test_pad_spex_input():
+    """ Function testing the import of the data for the spatial extent.
+    """
+
+    tau_data, regional_averages, regional_thresholds = datasets.pad_spex_input()
+
+    assert len(tau_data) == 308, "Wrong number of participants in the .tsv. Should be 308."
+    assert tau_data['CTX_LH_ENTORHINAL_SUVR'].isnull().sum() == 0, "Some data is missing in the simulated data"
+    assert len(regional_averages) == 16, "Wrong number of rows in the regional averages"
+    assert len(regional_thresholds) == 16, "Wrong number of rows in the regional thresholds"
